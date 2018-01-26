496321c556fcc8dac64220efbe8c382fa844141a
C-Elegans <mtnolan2640@gmail.com>
2018-01-26 11:43:57 -0500

    Allow for selecting which signal to graph
    Scales graph to fit on screen

--- graphics.c
+++ graphics.c
@@ -7,6 +7,47 @@ static GtkWidget *listitem;
 static GtkWidget *drawingarea;
 static GdkPixmap *pixmap = NULL;
 static struct rawfile* rf = NULL;
+static void get_scale(double* data, size_t items, double* scale, double* offset){
+  double min = data[0];
+  double max = data[0];
+  for(size_t i=1;i<items;i++){
+    if(data[i] > max)
+      max = data[i];
+    if(data[i] < min)
+      min = data[i];
+  }
+  *scale = 0.9/(max-min);
+  *offset = (max+min)+ 0.5;
+}
+static void graph_data(GdkPixmap *pixmap, GtkWidget *widget,
+		       int vindex, GdkColor color){
+  gdk_draw_rectangle (pixmap,
+		      widget->style->white_gc,
+		      TRUE,
+		      0, 0,
+		      widget->allocation.width,
+		      widget->allocation.height);
+  gdk_color_alloc(gtk_widget_get_colormap(widget),&color);
+  GdkGC* gc = gdk_gc_new(pixmap);
+  gdk_gc_set_foreground(gc,&color);
+  double* data = rf->variables[vindex].data;
+  int x =0,y=0;
+  int x_prev, y_prev;
+  double scale, offset;
+  get_scale(data, rf->points, &scale, &offset);
+  for(size_t i=0;i<rf->points;i++){
+    x = (int) (((double)i/rf->points) * widget->allocation.width);
+    double val = -scale * data[i] + offset;
+    y = val *widget->allocation.height;
+    if(i!=0){
+      gdk_draw_line(pixmap, gc, x_prev, y_prev, x,y);
+    }
+    y_prev = y;
+    x_prev = x;
+  }
+  gtk_widget_queue_draw_area(widget,0,0,widget->allocation.width, widget->allocation.height);
+
+}
 static gboolean configure_event( GtkWidget *widget, GdkEventConfigure *event){
   if (pixmap)
     g_object_unref(pixmap);
@@ -21,27 +62,6 @@ static gboolean configure_event( GtkWidget *widget, GdkEventConfigure *event){
 		      0, 0,
 		      widget->allocation.width,
 		      widget->allocation.height);
-  GdkColor color;
-  color.red = 65535;
-  color.green = 0;
-  color.blue = 0;
-  gdk_color_alloc(gtk_widget_get_colormap(widget),&color);
-  GdkGC* gc = gdk_gc_new(pixmap);
-  gdk_gc_set_foreground(gc,&color);
-  double* data = rf->variables[1].data;
-  int x =0,y=0;
-  int x_prev, y_prev;
-  double scale = 1.0;
-  for(size_t i=0;i<rf->points;i++){
-    x = (int) (((double)i/rf->points) * widget->allocation.width);
-    double val = scale * data[i]/2.0 + 0.5;
-    y = val *widget->allocation.height;
-    if(i!=0){
-      gdk_draw_line(pixmap, gc, x_prev, y_prev, x,y);
-    }
-    y_prev = y;
-    x_prev = x;
-  }
 
   return TRUE;
 }
@@ -62,11 +82,15 @@ static void list_item_selected(GtkWidget *gtklist, gpointer data){
   }
   while(dlist){
     GtkObject *item;
-    gchar* item_string;
+    gint item_index;
     item = GTK_OBJECT(dlist->data);
-    item_string = gtk_object_get_data(item,list_data_key);
-    g_print("%s\n", item_string);
-
+    item_index = GPOINTER_TO_INT(gtk_object_get_data(item,list_data_key));
+    printf("%d\n", item_index);
+    GdkColor color;
+    color.red = 65535;
+    color.green =0;
+    color.blue = 0;
+    graph_data(pixmap, drawingarea, item_index, color);
     dlist=dlist->next;
   }
 }
@@ -104,7 +128,7 @@ void graphics_run(struct rawfile* _rf){
     gtk_container_add(GTK_CONTAINER(listitem), label);
     gtk_widget_show(label);
     gtk_container_add(GTK_CONTAINER(list), listitem);
-    gtk_object_set_data(GTK_OBJECT(listitem), list_data_key, text);
+    gtk_object_set_data(GTK_OBJECT(listitem), list_data_key, GINT_TO_POINTER(i));
     gtk_widget_show(listitem);
   }
 
